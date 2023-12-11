28d2b0a9ae95e932f2706c34799e349cec45eebe
54718 <54718@etu.he2b.be>
2023-12-11 14:12:11 +0100

    test

--- hub/resources/views/courses.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('canevas')
-
-@section('title', 'Cours')
-
-@section('content')
-    <div class="container">
-        <p>Cours</p>
-    </div>
-@endsection
--- resources/views/lessons.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('canevas')
-
-@section('title', 'Leçons')
-
-@section('content')
-    <div class="container">
-        <p>Leçons</p>
-    </div>
-@endsection
